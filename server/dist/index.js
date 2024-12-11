b9dd828d62816b1ce19d42d330f864fb80459e54
Tanishka <tanishkadeep09@gmail.com>
2024-12-11 22:22:49 +0530

    implemented template endpoint

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,41 +8,56 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
+const express_1 = __importDefault(require("express"));
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
+const prompts_1 = require("./prompts");
+const cors_1 = __importDefault(require("cors"));
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const API_KEY = process.env.GEMINI_API_KEY;
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        var _a, e_1, _b, _c;
-        const genAI = new GoogleGenerativeAI(API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const userPrompt = "Explain how AI works";
-        // const result = await model.generateContent(userPrompt);
-        // console.log(result.response.text());
-        const result = yield model.generateContentStream(userPrompt);
-        try {
-            for (var _d = true, _e = __asyncValues(result.stream), _f; _f = yield _e.next(), _a = _f.done, !_a; _d = true) {
-                _c = _f.value;
-                _d = false;
-                const chunk = _c;
-                const chunkText = chunk.text();
-                process.stdout.write(chunkText);
-            }
-        }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
-            try {
-                if (!_d && !_a && (_b = _e.return)) yield _b.call(_e);
-            }
-            finally { if (e_1) throw e_1.error; }
-        }
-    });
-}
-main();
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userPrompt = req.body.prompt;
+    const prompt = "Here is the user prompt: " + userPrompt + "\n\n" + "Based on this prompt, Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra";
+    const result = yield model.generateContent(prompt);
+    const answer = result.response.text().trim(); // react or node
+    if (answer == "react") {
+        res.json({
+            prompts: [prompts_1.BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [react_1.basePrompt]
+        });
+        return;
+    }
+    if (answer == "node") {
+        res.json({
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${node_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [node_1.basePrompt]
+        });
+        return;
+    }
+    res.status(403).json({ message: "You cant access this" });
+    return;
+}));
+app.listen(3000);
+// async function main() {
+//   const genAI = new GoogleGenerativeAI(API_KEY);
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+//   const userPrompt = "Explain how AI works";
+//   // const result = await model.generateContent(userPrompt);
+//   // console.log(result.response.text());
+//   const result = await model.generateContentStream(userPrompt);
+//   for await (const chunk of result.stream) {
+//     const chunkText = chunk.text();
+//     process.stdout.write(chunkText);
+//   }
+// }
+// main();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,69 @@
 require("dotenv").config();
+import express from "express";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import cors from "cors";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-async function main() {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const userPrompt = "Explain how AI works";
-    // const result = await model.generateContent(userPrompt);
-    // console.log(result.response.text());
+const app = express();
+app.use(cors())
+app.use(express.json());
 
-    const result = await model.generateContentStream(userPrompt); 
+app.post("/template", async (req, res) => {
+  const userPrompt = req.body.prompt;
 
-    for await (const chunk of result.stream) {
-        const chunkText = chunk.text();
-        process.stdout.write(chunkText);
-      }
+  const prompt = "Here is the user prompt: " + userPrompt + "\n\n" + "Based on this prompt, Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra";
 
-}
+  const result = await model.generateContent(prompt);
 
-main();
+  const answer = result.response.text().trim(); // react or node
+
+  if (answer == "react") {
+    res.json({
+      prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+      uiPrompts: [reactBasePrompt]
+    })
+    return;
+  }
+
+  if (answer == "node") {
+    res.json({
+      prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+      uiPrompts: [nodeBasePrompt]
+    })
+    return;
+  }
+
+  res.status(403).json({ message: "You cant access this" })
+  return;
+
+
+});
+
+app.listen(3000);
+
+// async function main() {
+//   const genAI = new GoogleGenerativeAI(API_KEY);
+//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+//   const userPrompt = "Explain how AI works";
+//   // const result = await model.generateContent(userPrompt);
+//   // console.log(result.response.text());
+
+//   const result = await model.generateContentStream(userPrompt);
+
+//   for await (const chunk of result.stream) {
+//     const chunkText = chunk.text();
+//     process.stdout.write(chunkText);
+//   }
+
+// }
+
+// main();
